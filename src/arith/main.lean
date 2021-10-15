ed0a9df4aebe7b98b8a6f9016673f8c86a71229d
1inguini <9647142@gmail.com>
2021-10-15 22:47:59 +0900

    initial commit

---
+++ src/arith/main.lean
@@ -0,0 +1,15 @@
+import system.io
+import data.buffer, data.buffer.parser
+
+namespace arith
+
+  namespace parser
+
+  end parser
+
+  def str : string := "hello fron arith"
+
+end arith
+
+def main : io unit :=
+  io.put_str_ln arith.str

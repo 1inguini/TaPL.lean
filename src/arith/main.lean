7cad4d72e95a76e4a6a2e11dfb5cc7108c323040
1inguini <9647142@gmail.com>
2021-10-16 01:19:19 +0900

    defined term

--- src/arith/main.lean
+++ src/arith/main.lean
@@ -4,6 +4,21 @@ import data.buffer.parser
 
 namespace arith
 
+  namespace ast
+
+    structure info := (index : nat)
+
+    inductive term : Type
+    | true : info -> term
+    | false : info -> term
+    | if_then_else : info -> term -> term -> term -> term
+    | is_zero : info -> term -> term
+    | zero : info -> term
+    | succ : info -> term -> term
+    | pred : info -> term -> term
+
+  end ast
+
   namespace parser
 
   end parser
